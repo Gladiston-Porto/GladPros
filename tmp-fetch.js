27dacda6bae4ec756c386b1fd81a7f1bc5d2fe91
Gladiston Porto <gladiston.porto@gladpros.com>
2025-08-26 00:33:01 -0500

    chore(propostas): point tmp-fetch to consolidated /api/propostas

--- tmp-fetch.js
+++ tmp-fetch.js
@@ -1,6 +1,6 @@
 (async()=>{
   try {
-    const res = await fetch('http://localhost:3001/api/propostas/simple');
+  const res = await fetch('http://localhost:3001/api/propostas');
     console.log('status', res.status);
     const txt = await res.text();
     console.log(txt.slice(0,1000));
